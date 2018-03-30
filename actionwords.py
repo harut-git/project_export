8c16b5eff816548aed3083aa68f9a6bb7b480f84
harut <harutyun.ghubasaryan@betconstruct.com>
2018-03-30 10:23:59 +0400

    tutorial

--- actionwords.py
+++ actionwords.py
@@ -6,12 +6,5 @@ class Actionwords:
         self.test = test
         self.driver = driver
 
-    def i_open(self, link=""):
-        self.driver.get(link)
-
-    def search_for(self, word="", css_id =""):
-        self.driver.find_element_by_id(css_id).send_keys(word)
-        self.driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[3]/center/input[1]').click()
-
-    def click_on_the_first_result(self, object=""):
-        self.driver.find_element_by_xpath(object).click()
+    def i_open(self, url=""):
+        self.driver.get(url)
--- test_project.py
+++ test_project.py
@@ -4,13 +4,10 @@ from actionwords import Actionwords
 from selenium import webdriver
 
 
-class TestSeleniumTutorial(unittest.TestCase):
+class TestTutorial(unittest.TestCase):
     def setUp(self):
         driver = webdriver.Chrome(executable_path="/home/harut/Documents/chromedriver")
         self.actionwords = Actionwords(self, driver)
 
-    def test_finding_a_site_on_google(self):
-        self.actionwords.i_open(link="http://google.ru")
-        self.actionwords.search_for(word="hiptest", css_id="lst-ib")
-        self.actionwords.click_on_the_first_result('//*[@id="rso"]/div[1]/div/div/div/div/h3/a')
-
+    def test_open_a_website(self):
+        self.actionwords.i_open(url="http://google.ru")
